a67ff5e565d184d63f738d16def1fc04eda3167f
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-05-11 13:06:16 -0400

    Advance Practice

---
+++ Esp_Sensor/Mqtt.hpp
@@ -0,0 +1,72 @@
+#pragma once
+#include <PubSubClient.h>
+#include "Network.hpp"
+
+class MqttConfig {
+    public:
+        const char* server;
+        const char* clientId;
+        void (*callback)(char*, uint8_t*, unsigned int);
+        int port;
+        MqttConfig(const char* server, const char* clientId, void (*callback)(char*, uint8_t*, unsigned int), int port) : server(server), clientId(clientId), callback(callback), port(port) {}
+};
+
+class MqttClient{
+    public:
+        MqttConfig* config;
+        PubSubClient* client;
+        NetworkHandler* networkHandler;
+        MqttClient(MqttConfig* config, NetworkHandler* networkHandler) : config(config), networkHandler(networkHandler) {
+            client = new PubSubClient(*networkHandler->getClient());
+            client->setServer(config->server, config->port);
+            client->setCallback(config->callback);
+        }
+        MqttClient() {}
+        void setAll(MqttConfig* config, NetworkHandler* networkHandler) {
+            this->config = config;
+            this->networkHandler = networkHandler;
+            client = new PubSubClient(*networkHandler->getClient());
+            client->setServer(config->server, config->port);
+            client->setCallback(config->callback);
+        }
+        ~MqttClient() {
+            delete client;
+        }
+        void reconnect() {
+            if (!client->connected()) {
+                Serial.print("Attempting MQTT connection...");
+                if (client->connect(config->clientId)) {
+                    Serial.println("connected");
+                } else {
+                    Serial.print("failed, rc=");
+                    Serial.print(client->state());
+                    Serial.println(" try again in 5 seconds");
+                    delay(5000);
+                }
+            }
+        }
+        void publish(const char* topic, const char* payload) {
+            if (client->connected()) {
+                client->publish(topic, payload);
+            } else {
+                Serial.println("MQTT client not connected. Reconnecting...");
+                reconnect();
+            }
+        }
+        void subscribe(const char* topic) {
+            if (client->connected()) {
+                client->subscribe(topic);
+            } else {
+                Serial.println("MQTT client not connected. Reconnecting...");
+                reconnect();
+            }
+        }
+        void loop() {
+            if (client->connected()) {
+                client->loop();
+            } else {
+                Serial.println("MQTT client not connected. Reconnecting...");
+                reconnect();
+            }
+        }
+};
---
+++ Esp_Sensor/Network.hpp
@@ -0,0 +1,63 @@
+#pragma once
+#include <WiFiClientSecure.h>
+#include <WiFi.h>
+#include "certificates.h"
+
+class NetworkConfig{
+    public:
+        const char* ssid;
+        const char* password;
+        WiFiClientSecure* client;
+        NetworkConfig(const char* ssid, const char* password) : ssid(ssid), password(password) {
+            client = new WiFiClientSecure();
+            client->setCACert(AWS_ROOT_CA_CERTIFICATE);
+            client->setPrivateKey(AWS_PRIVATE_KEY);
+            client->setCertificate(AWS_CLIENT_CERTIFICATE);
+        }
+        NetworkConfig(const char* ssid, const char* password) : ssid(ssid), password(password) {
+            client = new WiFiClientSecure();
+        }
+        NetworkConfig(const char* ssid, const char* password, const char* root_ca, const char* private_key, const char* client_cert) : ssid(ssid), password(password) {
+            client = new WiFiClientSecure();
+            client->setCACert(root_ca);
+            client->setPrivateKey(private_key);
+            client->setCertificate(client_cert);
+        }
+        ~NetworkConfig() {
+            delete client;
+        }
+};
+
+class NetworkHandler {
+    public:
+        NetworkConfig* config;
+        NetworkHandler(NetworkConfig* config) : config(config) {}
+        void connect() {
+            WiFi.begin(config->ssid, config->password);
+            while (WiFi.status() != WL_CONNECTED) {
+                delay(1000);
+                Serial.println("Connecting to WiFi...");
+            }
+            Serial.println("Connected to WiFi");
+        }
+        WiFiClientSecure* getClient() {
+            return config->client;
+        }
+        void reconnect() {
+            if (WiFi.status() != WL_CONNECTED) {
+                WiFi.disconnect();
+                WiFi.begin(config->ssid, config->password);
+                while (WiFi.status() != WL_CONNECTED) {
+                    delay(1000);
+                    Serial.println("Reconnecting to WiFi...");
+                }
+                Serial.println("Reconnected to WiFi");
+            }
+        }
+        void inicialize() {
+            WiFi.mode(WIFI_STA);
+            WiFi.setSleep(false);
+            WiFi.setAutoReconnect(true);
+            WiFi.persistent(true);
+        }
+};
